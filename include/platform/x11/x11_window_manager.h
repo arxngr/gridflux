9a14677ef64b537340167f84f5bd844b88ef5ab9
ardi-nugraha <0x4rd1@gmail.com>
2025-08-07 21:26:51 +0700

    feat: move interface function to header

--- include/platform/x11/x11_window_manager.h
+++ include/platform/x11/x11_window_manager.h
@@ -3,7 +3,15 @@
 
 #ifdef GF_PLATFORM_X11
 
+#include "../../../include/core/logger.h"
+#include "../../../include/utils/memory.h"
+#include "../../core/geometry.h"
 #include "../../core/interfaces.h"
+#include <X11/X.h>
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
 #include "x11_atoms.h"
 
 // X11 platform data
@@ -30,6 +38,35 @@ gf_error_code_t gf_x11_get_frame_extents(Display *display, Window window,
                                          int *left, int *right, int *top,
                                          int *bottom);
 
+static gf_error_code_t gf_x11_platform_init(gf_platform_interface_t *platform,
+                                            gf_display_t *display);
+static void gf_x11_platform_cleanup(gf_display_t display);
+static gf_error_code_t
+gf_x11_platform_get_windows(gf_display_t display,
+                            gf_workspace_id_t workspace_id,
+                            gf_window_info_t **windows, uint32_t *count);
+static gf_error_code_t gf_x11_platform_set_window_geometry(
+    gf_display_t display, gf_native_window_t window, const gf_rect_t *geometry,
+    gf_geometry_flags_t flags);
+static gf_error_code_t
+gf_x11_platform_move_window_to_workspace(gf_display_t display,
+                                         gf_native_window_t window,
+                                         gf_workspace_id_t workspace_id);
+static gf_error_code_t
+gf_x11_platform_unmaximize_window(gf_display_t display,
+                                  gf_native_window_t window);
+static gf_error_code_t gf_x11_platform_get_window_geometry(
+    gf_display_t display, gf_native_window_t window, gf_rect_t *geometry);
+static gf_workspace_id_t
+gf_x11_platform_get_current_workspace(gf_display_t display);
+static uint32_t gf_x11_platform_get_workspace_count(gf_display_t display);
+static gf_error_code_t gf_x11_platform_create_workspace(gf_display_t display);
+static gf_error_code_t gf_x11_platform_get_screen_bounds(gf_display_t display,
+                                                         gf_rect_t *bounds);
+static bool gf_x11_platform_is_window_valid(gf_display_t display,
+                                            gf_native_window_t window);
+static bool gf_x11_platform_is_window_excluded(gf_display_t display,
+                                               gf_native_window_t window);
 const char *gf_x11_detect_desktop_environment(void);
 
 #endif // GF_PLATFORM_X11
--- src/platform/x11/x11_window_manager.c
+++ src/platform/x11/x11_window_manager.c
@@ -1,48 +1,11 @@
-#ifdef GF_PLATFORM_X11
 
 #include "../../../include/platform/x11/x11_window_manager.h"
-#include "../../../include/core/logger.h"
-#include "../../../include/utils/memory.h"
-#include "../include/core/geometry.h"
-#include <X11/X.h>
-#include <X11/Xatom.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 // X11 platform implementation
-static gf_error_code_t gf_x11_platform_init(gf_platform_interface_t *platform,
-                                            gf_display_t *display);
-static void gf_x11_platform_cleanup(gf_display_t display);
-static gf_error_code_t
-gf_x11_platform_get_windows(gf_display_t display,
-                            gf_workspace_id_t workspace_id,
-                            gf_window_info_t **windows, uint32_t *count);
-static gf_error_code_t gf_x11_platform_set_window_geometry(
-    gf_display_t display, gf_native_window_t window, const gf_rect_t *geometry,
-    gf_geometry_flags_t flags);
-static gf_error_code_t
-gf_x11_platform_move_window_to_workspace(gf_display_t display,
-                                         gf_native_window_t window,
-                                         gf_workspace_id_t workspace_id);
-static gf_error_code_t
-gf_x11_platform_unmaximize_window(gf_display_t display,
-                                  gf_native_window_t window);
-static gf_error_code_t gf_x11_platform_get_window_geometry(
-    gf_display_t display, gf_native_window_t window, gf_rect_t *geometry);
-static gf_workspace_id_t
-gf_x11_platform_get_current_workspace(gf_display_t display);
-static uint32_t gf_x11_platform_get_workspace_count(gf_display_t display);
-static gf_error_code_t gf_x11_platform_create_workspace(gf_display_t display);
-static gf_error_code_t gf_x11_platform_get_screen_bounds(gf_display_t display,
-                                                         gf_rect_t *bounds);
-static bool gf_x11_platform_is_window_valid(gf_display_t display,
-                                            gf_native_window_t window);
-static bool gf_x11_platform_is_window_excluded(gf_display_t display,
-                                               gf_native_window_t window);
 
 static int gf_x11_error_handler(Display *display, XErrorEvent *error) {
   char error_text[256];
@@ -600,5 +563,3 @@ static bool gf_x11_platform_is_window_excluded(gf_display_t display,
 
   return false;
 }
-
-#endif // GF_PLATFORM_X11
