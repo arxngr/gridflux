77a13d45b65720b1c1d4970a92a7e6dc612685d1
arxngr <0x4rd1@gmail.com>
2025-12-21 23:49:25 +0700

    chore: remove redundant part

--- src/platform/kwin/kwin_backend.c
+++ src/platform/kwin/kwin_backend.c
@@ -3,6 +3,7 @@
 #include "../../utils/file.h"
 #include "core/types.h"
 #include "platform/x11/x11_window_manager.h"
+
 #include <dbus/dbus.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -25,43 +26,37 @@ gf_kwin_dbus_call (DBusConnection *conn, const char *method, const char *arg1,
                                         KWIN_DBUS_INTERFACE, method);
     if (!msg)
     {
-        GF_LOG_ERROR ("Failed to create D-Bus message for method: %s", method);
+        GF_LOG_ERROR ("[kwin] failed to create D-Bus message: %s", method);
         return -1;
     }
 
     dbus_message_iter_init_append (msg, &args);
+
     if (arg1)
-    {
         dbus_message_iter_append_basic (&args, DBUS_TYPE_STRING, &arg1);
-    }
     if (arg2)
-    {
         dbus_message_iter_append_basic (&args, DBUS_TYPE_STRING, &arg2);
-    }
 
     reply = dbus_connection_send_with_reply_and_block (conn, msg, KWIN_DBUS_TIMEOUT_MS,
                                                        &error);
+
     if (dbus_error_is_set (&error))
     {
-        GF_LOG_ERROR ("KWin D-Bus error for method '%s': %s", method, error.message);
+        GF_LOG_ERROR ("[kwin] D-Bus error (%s): %s", method, error.message);
         dbus_error_free (&error);
-        dbus_message_unref (msg);
-        return -1;
+        goto out;
     }
 
-    if (reply && dbus_message_iter_init (reply, &args))
+    if (reply && dbus_message_iter_init (reply, &args)
+        && dbus_message_iter_get_arg_type (&args) == DBUS_TYPE_INT32)
     {
-        if (dbus_message_iter_get_arg_type (&args) == DBUS_TYPE_INT32)
-        {
-            dbus_message_iter_get_basic (&args, &result);
-        }
+        dbus_message_iter_get_basic (&args, &result);
     }
 
-    dbus_message_unref (msg);
+out:
     if (reply)
-    {
         dbus_message_unref (reply);
-    }
+    dbus_message_unref (msg);
 
     return result;
 }
@@ -69,159 +64,117 @@ gf_kwin_dbus_call (DBusConnection *conn, const char *method, const char *arg1,
 gf_error_code_t
 gf_kwin_platform_init (gf_platform_interface_t *platform)
 {
+    gf_x11_platform_data_t *data;
+    DBusError error;
+    char *script_path = NULL;
+    int ret = -1;
+    int fd = -1;
+    char tmp_template[] = KWIN_TEMP_TEMPLATE;
+
     GF_LOG_INFO ("[kwin] init: starting");
 
     if (!platform || !platform->platform_data)
     {
-        GF_LOG_ERROR ("[kwin] init: invalid platform or platform_data");
+        GF_LOG_ERROR ("[kwin] init: invalid platform");
         return GF_ERROR_PLATFORM_ERROR;
     }
 
-    gf_x11_platform_data_t *data = (gf_x11_platform_data_t *)platform->platform_data;
+    data = (gf_x11_platform_data_t *)platform->platform_data;
+    data->kwin_dbus_conn = NULL;
+    data->kwin_script_name = NULL;
 
-    DBusError error;
     dbus_error_init (&error);
 
-    GF_LOG_INFO ("[kwin] init: connecting to session D-Bus...");
     data->kwin_dbus_conn = dbus_bus_get (DBUS_BUS_SESSION, &error);
-    if (dbus_error_is_set (&error))
+    if (dbus_error_is_set (&error) || !data->kwin_dbus_conn)
     {
-        GF_LOG_ERROR ("[kwin] init: D-Bus error: %s", error.message);
+        GF_LOG_ERROR ("[kwin] init: D-Bus connection failed: %s",
+                      error.message ? error.message : "unknown");
         dbus_error_free (&error);
-        return GF_ERROR_PLATFORM_ERROR;
+        goto fail;
     }
-
-    if (!data->kwin_dbus_conn)
-    {
-        GF_LOG_ERROR ("[kwin] init: D-Bus connection is NULL");
-        return GF_ERROR_PLATFORM_ERROR;
-    }
-
-    GF_LOG_INFO ("[kwin] init: D-Bus connected");
 
     data->kwin_script_name = strdup (KWIN_SCRIPT_NAME);
     if (!data->kwin_script_name)
-    {
-        GF_LOG_ERROR ("[kwin] init: strdup failed for script name");
-        dbus_connection_unref (data->kwin_dbus_conn);
-        data->kwin_dbus_conn = NULL;
-        return GF_ERROR_PLATFORM_ERROR;
-    }
+        goto fail;
 
-    char *script_path = NULL;
-    for (size_t i = 0; KWIN_SCRIPT_SEARCH_PATHS[i] != NULL; ++i)
+    for (size_t i = 0; KWIN_SCRIPT_SEARCH_PATHS[i]; i++)
     {
-        GF_LOG_INFO ("[kwin] init: checking script path: %s",
-                     KWIN_SCRIPT_SEARCH_PATHS[i]);
         if (access (KWIN_SCRIPT_SEARCH_PATHS[i], R_OK) == 0)
         {
             script_path = strdup (KWIN_SCRIPT_SEARCH_PATHS[i]);
-            if (!script_path)
-            {
-                GF_LOG_ERROR ("[kwin] init: strdup failed for script path");
-                gf_free (data->kwin_script_name);
-                data->kwin_script_name = NULL;
-                dbus_connection_unref (data->kwin_dbus_conn);
-                data->kwin_dbus_conn = NULL;
-                return GF_ERROR_PLATFORM_ERROR;
-            }
             break;
         }
     }
 
     if (!script_path)
+        goto fail;
+
+    ret = gf_kwin_dbus_call (data->kwin_dbus_conn, "isScriptLoaded",
+                             data->kwin_script_name, NULL);
+
+    if (ret == 1)
     {
-        GF_LOG_ERROR ("[kwin] init: script not found in any search path");
-        gf_free (data->kwin_script_name);
-        data->kwin_script_name = NULL;
-        dbus_connection_unref (data->kwin_dbus_conn);
-        data->kwin_dbus_conn = NULL;
-        return GF_ERROR_PLATFORM_ERROR;
-    }
-
-    GF_LOG_INFO ("[kwin] init: using script path: %s", script_path);
-
-    GF_LOG_INFO ("[kwin] init: querying isScriptLoaded...");
-    int is_loaded = gf_kwin_dbus_call (data->kwin_dbus_conn, "isScriptLoaded",
-                                       data->kwin_script_name, NULL);
-    GF_LOG_INFO ("[kwin] init: isScriptLoaded returned %d", is_loaded);
-
-    if (is_loaded == 1)
-    {
-        GF_LOG_INFO ("[kwin] init: script already loaded, unloading...");
         gf_kwin_dbus_call (data->kwin_dbus_conn, "unloadScript", data->kwin_script_name,
                            NULL);
-        GF_LOG_INFO ("[kwin] init: unloadScript returned, sleeping...");
         sleep (KWIN_UNLOAD_SLEEP_SEC);
     }
 
-    char tmp_template[] = KWIN_TEMP_TEMPLATE;
-    GF_LOG_INFO ("[kwin] init: creating temp file...");
-    int fd = mkstemp (tmp_template);
+    fd = mkstemp (tmp_template);
     if (fd < 0)
-    {
-        GF_LOG_ERROR ("[kwin] init: mkstemp failed: %s", strerror (errno));
-        gf_free (script_path);
-        gf_free (data->kwin_script_name);
-        data->kwin_script_name = NULL;
-        dbus_connection_unref (data->kwin_dbus_conn);
-        data->kwin_dbus_conn = NULL;
-        return GF_ERROR_PLATFORM_ERROR;
-    }
+        goto fail;
     close (fd);
+    fd = -1;
 
-    GF_LOG_INFO ("[kwin] init: temp script path: %s", tmp_template);
-
-    GF_LOG_INFO ("[kwin] init: copying script...");
     if (gf_copy_file (script_path, tmp_template) != 0)
-    {
-        GF_LOG_ERROR ("[kwin] init: script copy failed");
-        unlink (tmp_template);
-        gf_free (script_path);
-        gf_free (data->kwin_script_name);
-        data->kwin_script_name = NULL;
-        dbus_connection_unref (data->kwin_dbus_conn);
-        data->kwin_dbus_conn = NULL;
-        return GF_ERROR_PLATFORM_ERROR;
-    }
+        goto fail;
 
-    GF_LOG_INFO ("[kwin] init: loading declarative script...");
-    int result = gf_kwin_dbus_call (data->kwin_dbus_conn, "loadDeclarativeScript",
-                                    tmp_template, data->kwin_script_name);
-    GF_LOG_INFO ("[kwin] init: loadDeclarativeScript returned %d", result);
+    ret = gf_kwin_dbus_call (data->kwin_dbus_conn, "loadDeclarativeScript", tmp_template,
+                             data->kwin_script_name);
 
-    if (result < 0)
-    {
-        GF_LOG_ERROR ("[kwin] init: loadDeclarativeScript failed");
-        unlink (tmp_template);
-        gf_free (script_path);
-        gf_free (data->kwin_script_name);
-        data->kwin_script_name = NULL;
-        dbus_connection_unref (data->kwin_dbus_conn);
-        data->kwin_dbus_conn = NULL;
-        return GF_ERROR_PLATFORM_ERROR;
-    }
+    if (ret < 0)
+        goto fail;
 
-    GF_LOG_INFO ("[kwin] init: starting script...");
     gf_kwin_dbus_call (data->kwin_dbus_conn, "start", NULL, NULL);
-    GF_LOG_INFO ("[kwin] init: start call returned");
 
     unlink (tmp_template);
-    gf_free (script_path);
+    free (script_path);
 
     GF_LOG_INFO ("[kwin] init: success");
     return GF_SUCCESS;
+
+fail:
+    if (fd >= 0)
+        close (fd);
+    unlink (tmp_template);
+
+    if (script_path)
+        free (script_path);
+
+    if (data->kwin_script_name)
+    {
+        free (data->kwin_script_name);
+        data->kwin_script_name = NULL;
+    }
+
+    if (data->kwin_dbus_conn)
+    {
+        dbus_connection_unref (data->kwin_dbus_conn);
+        data->kwin_dbus_conn = NULL;
+    }
+
+    return GF_ERROR_PLATFORM_ERROR;
 }
 
 void
 gf_kwin_platform_cleanup (gf_platform_interface_t *platform)
 {
-    if (!platform || !platform->platform_data)
-    {
-        return;
-    }
+    gf_x11_platform_data_t *data;
 
-    gf_x11_platform_data_t *data = (gf_x11_platform_data_t *)platform->platform_data;
+    if (!platform || !platform->platform_data)
+        return;
+
+    data = (gf_x11_platform_data_t *)platform->platform_data;
 
     if (data->kwin_dbus_conn && data->kwin_script_name)
     {
@@ -231,7 +184,7 @@ gf_kwin_platform_cleanup (gf_platform_interface_t *platform)
 
     if (data->kwin_script_name)
     {
-        gf_free (data->kwin_script_name);
+        free (data->kwin_script_name);
         data->kwin_script_name = NULL;
     }
 
