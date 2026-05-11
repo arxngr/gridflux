9db5246db348e96cc18598afd9274e6853c3b186
Ardi Nugraha <0x4rd1@gmail.com>
2026-05-11 13:06:51 +0700

    fix (unix): make dock restore / hide to synchronous process

--- src/platform/unix/internal.h
+++ src/platform/unix/internal.h
@@ -6,7 +6,7 @@
 #include <X11/Xatom.h>
 
 // --- Shell & Background ---
-void _run_bg_command (const char *cmd, char *const argv[]);
+void _run_cmd_sync (const char *cmd, char *const argv[]);
 
 // --- Window Identification & State ---
 bool _window_screenshot_app (gf_display_t display, gf_handle_t window);
@@ -19,7 +19,8 @@ bool _window_has_type (gf_display_t display, gf_handle_t window, Atom type);
 bool _window_name_matches (const char *name, const char *list[], size_t count);
 bool _window_it_self (gf_display_t display, gf_handle_t window);
 bool _window_excluded_border (gf_display_t display, gf_handle_t window);
-void gf_window_get_class (gf_display_t dpy, gf_handle_t win, char *buffer, size_t bufsize);
+void gf_window_get_class (gf_display_t dpy, gf_handle_t win, char *buffer,
+                          size_t bufsize);
 
 // --- Border Management ---
 void gf_border_add (gf_platform_t *platform, gf_handle_t window, gf_color_t color,
